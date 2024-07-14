5bbe1b7a855fab248712d589a96cb05cb975b45f
yogeshwar213 <yogeshwarkawade213@gmail.com>
2024-07-14 20:02:51 +0530

    initial commit

---
+++ server/config/database.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const {MONGOBD_URL} = process.env;
+
+exports.connect = () => {
+	mongoose.connect(MONGOBD_URL, {
+			useNewUrlparser: true,
+			useUnifiedTopology: true,
+		})
+		.then(console.log(`DB Connection Success`))
+		.catch((err) => {
+			console.log(`DB Connection Failed`);
+			console.log(err);
+			process.exit(1);
+		});
+};
